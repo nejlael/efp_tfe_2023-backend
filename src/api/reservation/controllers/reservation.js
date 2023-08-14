110ad4a8f983a820876716b5e4d23c37eeef955e
Nejla El morabet <elmorabet.n@hotmail.com>
2023-08-14 18:00:25 +0200

    ca marche pas mit en commentaire

--- src/api/reservation/controllers/reservation.js
+++ src/api/reservation/controllers/reservation.js
@@ -1,16 +1,16 @@
-const ReservationController = {
-    create: async (ctx) => {
-      const { courseID, numeroClient } = ctx.request.body;
-  
-      // Créer une nouvelle réservation
-      const reservation = await strapi.services.reservation.create({
-        numeroClient: numeroClient,
-        courseID: courseID,
-      });
-  
-      // Envoyer une réponse de succès
-      return ctx.send({ message: 'Réservation effectuée avec succès' });
-    },
+/* const ReservationController = {
+  create: async (ctx) => {
+    const { courseID, numeroClient } = ctx.request.body;
+
+    // Créer une nouvelle réservation
+    const reservation = await strapi.services.reservation.create({
+      numeroClient: numeroClient,
+      courseID: courseID,
+    });
+
+    // Envoyer une réponse de succès
+    return ctx.send({ message: 'Réservation effectuée avec succès' });
+  },
 };
-  
-module.exports = ReservationController;
+
+module.exports = ReservationController; */
